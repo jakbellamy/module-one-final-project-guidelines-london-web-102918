6a2dffeed4168d322779efb299bb08b05b1f02bf
seeduardo <romenflatiron@gmail.com>
2018-11-14 18:45:33 +0000

    added beer_me method to cli

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -12,3 +12,8 @@ def list_beer_styles
     "#{s.name} (catalogue number: #{s.id})"
   end
 end
+
+def beer_me
+  random_beer = Beer.all.sample
+  "Too lazy (or drunk) to choose your own beer? No problem! Why don't you try the #{random_beer.name} made by #{random_beer.brewery.name}? With an ABV of #{random_beer.abv}, it's sure to warm your cockles (if that's your thing)!"
+end

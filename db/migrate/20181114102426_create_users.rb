54dc67cd0cb9d4dda4d8db7da5e71e08c23d25ae
seeduardo <romenflatiron@gmail.com>
2018-11-14 10:26:42 +0000

    deleted test db file and added users table migration

---
+++ db/migrate/20181114102426_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user do |t|
+      t.string :name
+      t.string :city
+      t.string :country
+      t.integer :age
+    end
+  end
+end

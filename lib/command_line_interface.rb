9e88dc2acabad4663463f79d492c51aa0818755c
seeduardo <romenflatiron@gmail.com>
2018-11-14 09:42:33 +0000

    Added First Attempt at Welcome and Login

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,9 @@
+def welcome
+  puts "Welcome to BeerMe, oh seeker of beer and all its attendant delights!"
+end
+
+
+def login_prompt
+  puts "Please enter your user name"
+  gets.chomp.downcase
+end

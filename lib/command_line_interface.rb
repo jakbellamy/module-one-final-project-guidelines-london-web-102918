aa96bd360ff6da06a428ae9061c7665b0ad761fe
seeduardo <romenflatiron@gmail.com>
2018-11-14 18:25:15 +0000

    added list_beer_styles method to cli

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,8 +2,13 @@ def welcome
   puts "Welcome to BeerMe, oh seeker of beer and all its attendant delights!"
 end
 
-
 def login_prompt
   puts "Please enter your user name"
   gets.chomp.downcase
+end
+
+def list_beer_styles
+  Style.all.map do |s|
+    "#{s.name} (catalogue number: #{s.id})"
+  end
 end

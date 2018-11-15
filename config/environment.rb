4ccac8a0ed8090f6bd35b888eedecca597a1f91e
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-15 14:21:56 +0000

    made all beer finder methods work if user input is correct

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ require 'bundler'
 require 'active_record'
 Bundler.require
 
+ActiveRecord::Base.logger = nil
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
 require_all 'app'
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -23,7 +23,6 @@ def main_menu_options_prompt
   puts "Please type 'main' at any time to return to the MAIN MENU"
 end
 
-<<<<<<< HEAD
 def main_menu_options
   main_menu_options_prompt
   user_input = gets.chomp
@@ -41,10 +40,6 @@ def main_menu_options
     else
       invalid_input
   end
-=======
-def help
-  help_list_of_commands
->>>>>>> finish-find-a-beer
 end
 
 #----STYLES----
@@ -63,20 +58,6 @@ def beer_me
 end
 
 #----BREWERY METHODS----
-<<<<<<< HEAD
-#
-# def brewery_search_request_help
-#   puts "Type '1' to search by Brewery Name"
-#   puts  "Type '2' to search by your location"
-#   puts "Type '3' to search  by a new Location"
-# end
-#
-# def brewery_search_request_test(user_input)
-#
-#   case brew
-#     if brewery_search_request = 1
-#         Brewery.find_by(name)
-=======
 
 # def brewery_search_request_help
 #   puts "Type '1' to search by Brewery Name"
@@ -89,7 +70,6 @@ end
 #   when brewery_search_request == 1
 #         Brewery.find_by(name)
 #   end
->>>>>>> finish-find-a-beer
 # end
 
 # def get_user_input
@@ -137,22 +117,24 @@ def beer_by_name
 end
 
 def beer_by_brewery
-  "Please enter the name of a brewery to see their beers"
+  puts "Please enter the name of a brewery to see their beers"
   user_input = gets.chomp
-  beers = Beer.select {|beer| beer.brewery.name == user_input}
-  puts beers
+  brewery = Brewery.find_by(name: user_input)
+  beers = brewery.beers
+  beers.each {|beer| puts "#{beer.name}, #{beer.style.name}, #{beer.abv}, #{beer.brewery.name}"}
 end
 
 def beer_by_abv
-  "Please enter an abv to see beers with a similar abv"
+  puts "Please enter an abv to see ten beers with a similar abv"
   user_input = gets.chomp.to_f
-  beers = Beer.select {|beer| beer.abv == (user_input)} # between?(user_input +0.5, user_input -0.5)
-  beers.first(10)
-  beers.map {|beer| puts "beer.name, beer.brewery.name, beer.abv"}
+  beers = Beer.all.select {|beer| beer.abv == user_input}.sample(10) # (user_input >= 1 || user_input <= 1)}
+  beers.each {|beer| puts "#{beer.name}, #{beer.style.name}, #{beer.abv}, #{beer.brewery.name}"}
 end
 
 def beer_by_style
-  user_input = gets.chomp.downcase.capitalize
-  beers = Beer.select {|beer| beer.style == user_input}
-  beers.first(10)
+  puts "Please enter the name of a style to see beers of that style"
+  user_input = gets.chomp
+  style = Style.find_by(name: user_input)
+  beers = style.beers
+  beers.each {|beer| puts "#{beer.name}, #{beer.style.name}, #{beer.abv}, #{beer.brewery.name}"}
 end

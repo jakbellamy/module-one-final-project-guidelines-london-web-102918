03121ae43d97944771492a20374551e200a0dc2a
kbberker <kbbuk90@gmail.com>
2018-11-15 13:41:14 +0000

    created brewery_methods.rb file to lib folder

---
+++ lib/brewery_methods.rb
@@ -0,0 +1,15 @@
+#----BREWERY METHODS----
+
+# def brewery_search_request_help
+#   puts "Type '1' to search by Brewery Name"
+#   puts "Type '2' to search by your location"
+#   puts "Type '3' to search  by a new location"
+# end
+#
+# def brewery_search_request_test(user_input)
+#   case brew
+#   when brewery_search_request == 1
+#         Brewery.find_by(name)
+#   end
+# end
+

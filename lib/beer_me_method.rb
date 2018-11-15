7d64b2a3d058e2652cb41ab73059993d577a9dee
seeduardo <romenflatiron@gmail.com>
2018-11-15 18:14:33 +0000

    added extra beer_me_again methods to beer_me_method.rb

--- lib/beer_me_method.rb
+++ lib/beer_me_method.rb
@@ -3,7 +3,40 @@
 def beer_me
   puts "- - - - - - - - - - - - -"
   random_beer = Beer.all.sample
-  puts "Too lazy (or drunk) to choose your own beer? No problem! Why don't you try the #{random_beer.name} made by #{random_beer.brewery.name}? With an ABV of #{random_beer.abv}, it's sure to warm your cockles (if that's your thing)!"
+  puts "Too lazy (or drunk) to choose your own beer?"
+  puts "No problem! Why don't you try the #{random_beer.name} made by #{random_beer.brewery.name}?"
+  puts "With an ABV of #{random_beer.abv}, it's sure to warm your cockles"
+  puts "(if that's your thing)!"
   puts "- - - - - - - - - - - - -"
-  main_menu_options
+  puts "And if that particular brew was NOT quite your thing..."
+  beer_me_again_options
+end
+
+def beer_me_again
+  puts "- - - - - - - - - - - - -"
+  random_beer = Beer.all.sample
+  puts "Well then choosy!"
+  puts "Instead you could try the #{random_beer.name} made by #{random_beer.brewery.name}. It's abv is #{random_beer.abv}, but don't let that stop you!"
+  puts "- - - - - - - - - - - - -"
+  beer_me_again_options
+end
+
+def beer_me_again_prompt
+  puts "...you could just type 'BeerMe' to go for it one more time!"
+  puts "Otherwise, please type 'main' to return to the Main Menu"
+end
+
+def beer_me_again_options
+  beer_me_again_prompt
+  puts "Type here:"
+  user_input = gets.chomp.downcase
+  case user_input
+  when "beerme"
+    beer_me_again
+  when "main"
+    main_menu_options
+  else
+    invalid_input
+  end
+  beer_me_again_options
 end

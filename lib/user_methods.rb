730b2b2682462fc47e24b5454390a5466a1bbfd5
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-15 16:23:36 +0000

    added user_methods file, detailed minimum methods for it to work

---
+++ lib/user_methods.rb
@@ -0,0 +1,23 @@
+#----USER METHODS----
+
+# user sees the general welcome message, gets prompted to log in or create new user
+
+# log in asks for name or email and validates input
+# if no record found suggest checking spelling or creating a new account
+# if user found asks for password
+# if password is correct goes to main menu
+# if not correct suggest checking spelling or creating a new account
+
+# create account asks for user name, email and password
+# once account created goes to main menu
+
+# personalised welcome
+# main menu options now including including log out option which re-runs the whole file
+
+# methods below are inside the my account menu branch of the main menu
+# start with most basic methods - adding to user_beers by name to create the join between beers and users
+
+# see user_beers - realitively easy
+# add to user_beers harder - how?
+
+# add beer by name

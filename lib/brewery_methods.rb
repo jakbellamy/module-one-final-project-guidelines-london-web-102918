915501faedd336fa52bac4c3559a2fb413660451
kbberker <kbbuk90@gmail.com>
2018-11-15 15:53:27 +0000

    added brewery search options and matched find_brewery to main menu call

--- lib/brewery_methods.rb
+++ lib/brewery_methods.rb
@@ -1,17 +1,18 @@
 #----BREWERY METHODS----
 
-def brewery_search_request_help
+def brewery_search_options
   puts "Type '1' to search by Brewery Name"
   puts "Type '2' to search by your location"
   puts "Type '3' to search  by a new location"
 end
 
 
-def brewery_search_request_test(get_user_input)
-  case brew
-  when brewery_search_request == 1
-        Brewery.find_by(name)
-  end
+def find_brewery
+    brewery_search_options
+    # case brew
+    # when brewery_search_request == 1
+    #         Brewery.find_by(name)
+    # end
 end
 
 

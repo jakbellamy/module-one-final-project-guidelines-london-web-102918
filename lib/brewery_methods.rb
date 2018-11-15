4c39b6b332384e4af351afc0fcef298add240689
kbberker <kbbuk90@gmail.com>
2018-11-15 14:27:36 +0000

    added brewery_methods.rb

--- lib/brewery_methods.rb
+++ lib/brewery_methods.rb
@@ -1,15 +1,24 @@
 #----BREWERY METHODS----
 
-# def brewery_search_request_help
-#   puts "Type '1' to search by Brewery Name"
-#   puts "Type '2' to search by your location"
-#   puts "Type '3' to search  by a new location"
-# end
-#
-# def brewery_search_request_test(user_input)
-#   case brew
-#   when brewery_search_request == 1
-#         Brewery.find_by(name)
-#   end
-# end
+def brewery_search_request_help
+  puts "Type '1' to search by Brewery Name"
+  puts "Type '2' to search by your location"
+  puts "Type '3' to search  by a new location"
+end
 
+
+def brewery_search_request_test(get_user_input)
+  case brew
+  when brewery_search_request == 1
+        Brewery.find_by(name)
+  end
+end
+
+
+
+
+#----KENAN PRIVATE METHODS----
+
+def get_user_input
+  gets.chomp
+end

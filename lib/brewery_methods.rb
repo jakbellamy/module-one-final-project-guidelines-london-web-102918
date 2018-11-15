cbfb26cb40c719f78f48beea7d51499dc38ebe20
kbberker <kbbuk90@gmail.com>
2018-11-15 17:36:18 +0000

    Added brewery_by_name method

--- lib/brewery_methods.rb
+++ lib/brewery_methods.rb
@@ -8,24 +8,35 @@ end
 
 
 def find_brewery
-    brewery_search_options
-    puts "Type here:"
-    user_input = gets.chomp
-    case user_input
-    when "1"
-        brewery_by_name
-    when "2"
-        brewery_by_user_loc
-    when "3"
-        brewery_by_location
-    when "main"
-        main_menu_options
-    else
-        invalid_input
-    end
+  brewery_search_options
+  puts "Type here:"
+  user_input = gets.chomp
+  case user_input
+  when "1"
+    brewery_by_name
+  when "2"
+    brewery_by_user_loc
+  when "3"
+    brewery_by_location
+  when "main"
+    main_menu_options
+  else
+    invalid_input
+  end
 end
 
-
+def brewery_by_name
+  puts "- - - - - - - - - - - - -"
+  puts "Please enter the name of a brewery"
+  puts "- - - - - - - - - - - - -"
+  puts "Type here:"
+  user_input = gets.chomp
+  brewery = Brewery.find_by(name: user_input)
+  puts "- - - - - - - - - - - - -"
+  puts "#{brewery.name} is in #{brewery.city}, #{brewery.country}. Find out more from their website #{brewery.url}"
+  puts "- - - - - - - - - - - - -"
+  main_menu_options
+end
 
 
 #----KENAN PRIVATE METHODS----

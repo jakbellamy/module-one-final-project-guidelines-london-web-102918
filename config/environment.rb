cfc161753d7c2685ca796ccb34302f7cd9563e6c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-16 09:59:25 +0000

    fixed error messages when running file and program now updates user_beers automatically

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,6 @@ Bundler.require
 ActiveRecord::Base.logger = nil
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
 require_all 'app'
+
+require_all 'lib'

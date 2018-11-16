0344d5e51be7a33fc3837135f54528280f32dbaf
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-16 12:09:23 +0000

    added thanks page

---
+++ lib/thanks_page.rb
@@ -0,0 +1,16 @@
+def thanks
+  puts"
+██████╗ ███████╗███████╗██████╗ ███╗   ███╗███████╗
+██╔══██╗██╔════╝██╔════╝██╔══██╗████╗ ████║██╔════╝
+██████╔╝█████╗  █████╗  ██████╔╝██╔████╔██║█████╗
+██╔══██╗██╔══╝  ██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══╝
+██████╔╝███████╗███████╗██║  ██║██║ ╚═╝ ██║███████╗
+╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝"
+  puts "Made by Kenan, Paul and Romén"
+  puts "Thanks to Arti for the unicode"
+  puts "Thanks to Paolo for the global variable"
+  puts "Thanks to Marie-anne (Paul's girlfrend) for saying that the app is sh\u{1F37A}t"
+
+
+main_menu_options
+end

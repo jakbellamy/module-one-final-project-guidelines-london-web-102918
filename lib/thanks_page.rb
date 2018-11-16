786a92571e5742000693c1fa28cfb5bb552875e1
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-16 14:06:22 +0000

    updated thanks page content and now have to press enter to go back to main menu

--- lib/thanks_page.rb
+++ lib/thanks_page.rb
@@ -1,16 +1,20 @@
 def thanks
   puts"
-██████╗ ███████╗███████╗██████╗ ███╗   ███╗███████╗
-██╔══██╗██╔════╝██╔════╝██╔══██╗████╗ ████║██╔════╝
-██████╔╝█████╗  █████╗  ██████╔╝██╔████╔██║█████╗
-██╔══██╗██╔══╝  ██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══╝
-██████╔╝███████╗███████╗██║  ██║██║ ╚═╝ ██║███████╗
-╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝"
-  puts "Made by Kenan, Paul and Romén"
-  puts "Thanks to Arti for the unicode"
-  puts "Thanks to Paolo for the global variable"
-  puts "Thanks to Marie-anne (Paul's girlfrend) for saying that the app is sh\u{1F37A}t"
-
-
-main_menu_options
+        ██████╗ ███████╗███████╗██████╗ ███╗   ███╗███████╗
+        ██╔══██╗██╔════╝██╔════╝██╔══██╗████╗ ████║██╔════╝
+        ██████╔╝█████╗  █████╗  ██████╔╝██╔████╔██║█████╗
+        ██╔══██╗██╔══╝  ██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══╝
+        ██████╔╝███████╗███████╗██║  ██║██║ ╚═╝ ██║███████╗
+        ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝"
+  puts ""
+  puts "\u{1F37B} Made by Kenan, Paul and Romén"
+  puts "\u{1F37B} Thanks to Arti for the unicode"
+  puts "\u{1F37B} Thanks to Paolo and Rosen for the global variable"
+  puts "\u{1F37B} Thanks to Lucy, Alice, Dan and Sam for being such great teachers"
+  puts "\u{1F37B} Thanks to https://openbeerdb.com/ for the CSV files used for our database"
+  puts "\u{1F37B} Thanks to Marie-anne (Paul's girlfriend) for saying that the app is sh\u{1F37A}t"
+  puts ""
+  puts "Please press enter to go back to the home page"
+  gets.chomp
+  main_menu_options
 end
